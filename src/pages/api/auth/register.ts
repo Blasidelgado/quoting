4e5c10be1c13ddc4fcca051cefc3f337bbb76506
Blasidelgado <blasidelgado@gmail.com>
2023-11-03 10:03:45 -0300

    Fix: /api/auth/register working as intended

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -1,48 +1,46 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import { hash } from "bcryptjs"
-import { connectToDatabase } from "../../../../lib/mongodb"
-import User from "../../../../models/user"
-import { IUser } from "../../../../types"
+import { NextApiRequest, NextApiResponse } from "next";
+import { hash } from "bcryptjs";
+import { connectToDatabase } from "../../../../lib/mongodb";
+import User from "../../../../models/user";
+import { IUser } from "../../../../types";
 
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    connectToDatabase().catch(err => res.json(err))
+    connectToDatabase().catch(err => res.json(err));
 
     if (req.method === "POST") {
-        if (!req.body) return res.status(400).json({ error: "Data is missing" })
 
         const { name, password } = req.body;
 
-        const userExists = await User.findOne({ name })
+        if (!name || !password) {
+            return res.status(400).json({ error: "Data is missing" });
+        }    
+
+        const userExists = await User.findOne({ name });
 
         if (userExists) {
-            return res.status(409).json({ error: "User Already exists" })
+            return res.status(409).json({ error: "User Already exists" });
         }
         else {
             if (password.length < 6)
-                return res.status(409).json({ error: "Password should be 6 characters long" })
+                return res.status(409).json({ error: "Password should be 6 characters long" });
 
-            const hashedPassword = await hash(password, 12)
+            const hashedPassword = await hash(password, 12);
 
             User.create({
                 name,
                 password: hashedPassword
-            }, (error: unknown, data: IUser) => {
-                const user = {
-                    name: data.name,
-                    _id: data._id
-                }
+            });
 
-                return res.status(201).json({
-                    success: true,
-                    user
-                })
-            })
+            return res.status(201).json({
+                success: true,
+                name
+            });
         }
     }
     else {
-        res.status(405).json({ error: "Method Not Allowed" })
+        res.status(405).json({ error: "Method Not Allowed" });
     }
-}
+};
 
 export default handler
