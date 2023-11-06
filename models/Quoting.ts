d585bf9cedc911ecc10c1523971399ac379e0386
Blasidelgado <blasidelgado@gmail.com>
2023-11-05 23:51:50 -0300

    Created quoting model

---
+++ models/Quoting.ts
@@ -0,0 +1,51 @@
+import mongoose, { Schema, Document, models } from 'mongoose';
+
+interface QuotingConcept extends Document {
+  product: mongoose.Types.ObjectId;
+  subtotal: number;
+}
+
+export interface Quoting extends Document {
+  number: string;
+  date: Date;
+  client: mongoose.Types.ObjectId;
+  concepts: QuotingConcept[];
+  total: number;
+}
+
+const quotingConceptSchema: Schema = new Schema({
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  subtotal: {
+    type: Number,
+    required: true,
+  },
+});
+
+const quotingSchema: Schema = new Schema({
+  number: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  client: {
+    type: Schema.Types.ObjectId,
+    ref: 'Client',
+    required: true,
+  },
+  concepts: [quotingConceptSchema],
+  total: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Quoting = models.Quoting || mongoose.model<Quoting>('Quoting', quotingSchema);
+
+export default Quoting;
