735d96e4c3db90b2655343bb42de831e6cdddfad
Blasidelgado <blasidelgado@gmail.com>
2023-11-08 13:02:04 -0300

    minor changes

--- src/components/PriceList/PriceList.tsx
+++ src/components/PriceList/PriceList.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { IPriceList as PriceListType, PriceListItem as PriceListItemType} from '../../../types/priceList';
+import { IPriceList as PriceListType } from '../../../types/priceList';
 import { Product } from '../../../types/product';
 import PriceListItem from './PriceListItem';
 
@@ -15,20 +15,20 @@ export default function PriceList({ selectedList, products, handleUpdatePrice, o
 
     const handleEdit = (itemId) => {
         setActiveId(itemId);
-    }
+    };
 
     return (
-        <section className='mb-6'>
+        <section className='bg-white p-6 rounded-lg shadow-md mb-6'>
             {selectedList ? (
                 <>
-                    <h2 className='text-2xl font-bold mb-2'>{selectedList.priceListName}</h2>
-                    <button className="ml-4 text-red-500" onClick={() => onDelete(selectedList._id)}>
-                        Delete
+                    <h2 className='text-2xl font-bold mb-4'>{selectedList.priceListName}</h2>
+                    <button className="text-red-500 hover:underline mb-4" onClick={() => onDelete(selectedList._id)}>
+                        Eliminar lista
                     </button>
                     <ul>
-                        <li>
-                            <span>Producto: </span>
-                            <span>Precio: </span>
+                        <li className='font-semibold mb-2'>
+                            <span className='mr-4'>Producto</span>
+                            <span>Precio</span>
                         </li>
                         {selectedList.prices.map((item) => (
                             <PriceListItem
--- src/components/PriceList/SelectPriceList.tsx
+++ src/components/PriceList/SelectPriceList.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
-export default function SelectPriceList({allLists, selectedId, handleSelectedList }) {
+export default function SelectPriceList({ allLists, selectedId, handleSelectedList }) {
     return (
-        <select defaultValue={selectedId} onChange={(e) => handleSelectedList(e.target.value)}>
+        <select
+            defaultValue={selectedId}
+            onChange={(e) => handleSelectedList(e.target.value)}
+            className="w-full py-2 px-4 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+        >
             <option value='' hidden>Selecciona una lista</option>
             {allLists && allLists.map(list => {
                 return <option key={list._id} value={list._id}>{list.priceListName}</option>
             })}
         </select>
-    )
-};
+    );
+}
